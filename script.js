06146f8464385054f50a9ffc87ba6bdb8ba22d8d
Shivam Vishwakarma <shivam24581@gmail.com>
2025-04-21 22:09:14 +0530

    Update script.js
    remove localhost from fetching songs syntax

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ async function getSongs(folder) {
 
     currFolder = folder
 
-    let a = await fetch(`http://127.0.0.1:3000/${folder}/`)
+    let a = await fetch(`/${folder}/`)
     let response = await a.text();
 
     let div = document.createElement('div')
@@ -94,7 +94,7 @@ const playMusic = (track, pause = false) => {
 
 async function displayAlbums() {
 
-    let a = await fetch(`http://127.0.0.1:3000/songs`)
+    let a = await fetch(`/songs`)
     let response = await a.text();
     let div = document.createElement("div")
     div.innerHTML = response;
@@ -106,7 +106,7 @@ async function displayAlbums() {
         if (e.href.includes("/songs") && !e.href.includes(".htaccess")) {
             let folder = e.href.split("/").slice(-2)[0]
             // Get the metadata of the folder
-            let b = await fetch(`http://127.0.0.1:3000/songs/${folder}/info.json`)
+            let b = await fetch(`/songs/${folder}/info.json`)
             let response = await b.json();
             cardContainer.innerHTML = cardContainer.innerHTML + ` 
             <div data-folder="${folder}" class="card">
